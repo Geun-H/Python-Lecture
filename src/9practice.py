d17df0e028cad8ea01eacef7ee3a53e6e75aa84e
Heo Geun <gjrms365@gmail.com>
2024-07-29 14:32:31 +0900

    new

--- src/9practice.py
+++ src/9practice.py
@@ -5,4 +5,16 @@ res = 1
 while i > 0:
     res *= i
     i -= 1
-print(respython)
+print(res)
+
+
+n = 5
+result = 1
+while True:
+    if n == 0:
+        break
+
+    result *= n
+    n -= 1
+
+print(result)
